4f59991dbc8402f32a5f5184715c40f382e15a68
mehmetpekdemir <mehmetpekdemir06@hotmail.com>
2021-07-01 14:43:00 +0300

    initial commit.

---
+++ src/test/java/com/mehmetpekdemir/apachekafka/ApacheKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mehmetpekdemir.apachekafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApacheKafkaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
